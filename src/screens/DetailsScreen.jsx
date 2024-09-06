f58d289c002dac0923452745a113c6a424ea2d2e
dodimarthen <dodimartin.sit@gmail.com>
2024-09-06 15:31:10 +0700

    Add detail screen

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,29 +1,55 @@
 // screens/DetailsScreen.js
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+
+import { View, Text, TextInput } from "react-native";
+import { useFonts } from "expo-font";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const DetailsScreen = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+  });
+  const [pressed, setPressed] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Details Screen</Text>
-      <Button
-        title="Go back to Home"
-        onPress={() => navigation.navigate("Home")}
-      />
-    </View>
+    <SafeAreaView>
+      <View className="flex-1 bg-custom-white items-center justify-start min-h-screen">
+        <View className="items-center">
+          <Text
+            className="text-2xl text-custom-blue font-semibold mt-40 mb-0"
+            style={{
+              fontFamily: fontsLoaded ? "Roboto-Bold" : "System",
+            }}
+          >
+            Sign in To Your Account
+          </Text>
+        </View>
+        <View className="w-full px-8 mt-10">
+          {/* Username Container */}
+          <View className="mb-6">
+            <Text className="text-s text-slate-800 mb-2 font-bold">
+              Username
+            </Text>
+            <TextInput
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+              placeholder="Enter your username"
+            />
+          </View>
+
+          {/* Password Container */}
+          <View className="mb-6">
+            <Text className="text-s text-slate-800 mb-2 font-bold">
+              Password
+            </Text>
+            <TextInput
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+              placeholder="Enter your password"
+              secureTextEntry
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
 
 export default DetailsScreen;
