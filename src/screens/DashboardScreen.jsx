4aa3783c9f58e9981f200ed2d5808d444ed86e94
dodimarthen <dodimartin.sit@gmail.com>
2024-09-19 10:17:48 +0700

    adding calendar component

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Avatar } from "react-native-paper";
-import { Ionicons } from "@expo/vector-icons"; // Import Ionicons
 import { useFonts } from "expo-font";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CalendarComponent from "../components/Calendar/Calendar";
 
 const DashboardScreen = () => {
   const [fontsLoaded] = useFonts({
-    "Roboto-Light": require("../assets/fonts/Roboto-Medium.ttf"),
+    Montserrat: require("../assets/fonts/Montserrat-Medium.ttf"),
+    Roboto: require("../assets/fonts/Roboto-Bold.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -15,22 +16,26 @@ const DashboardScreen = () => {
   }
 
   return (
-    <SafeAreaView className="bg-custom-white">
-      <View className="flex-row items-center p-6 bg-custom-white">
+    <SafeAreaView className="flex-1 bg-white">
+      {/* Header Section */}
+      <View className="flex-row items-center p-4 mt-2">
         <Avatar.Image
-          size={55}
+          size={50}
           source={{
             uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4", // Replace with your image URL or local asset
           }}
         />
-        <View className="ml-4">
-          <Text className="text-lg font-extrabold">John Doe</Text>
-          <Text className="text-base text-gray-500">Software Engineer</Text>
+        <View className="ml-2">
+          <Text className="font-roboto text-lg font-bold">John Doe</Text>
+          <Text className="font-montserrat text-sm text-gray-500">
+            Software Engineer
+          </Text>
         </View>
+      </View>
 
-        <View className="ml-auto">
-          <Ionicons name="notifications-outline" size={32} color="black" />
-        </View>
+      {/* Calendar Section with same ml and mr */}
+      <View className="mt-5">
+        <CalendarComponent />
       </View>
     </SafeAreaView>
   );
