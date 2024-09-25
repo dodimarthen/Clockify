167f2f1dc4ef8b427d906c990566b48228ae14d3
dodimarthen <dodimartin.sit@gmail.com>
2024-09-25 10:47:25 +0700

    add new card for break time and calendars

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text } from "react-native";
 import { Avatar } from "react-native-paper";
 import { useFonts } from "expo-font";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -15,8 +15,10 @@ const DashboardScreen = () => {
 
   const [isCheckedIn, setIsCheckedIn] = React.useState(false);
   const [isCheckedOut, setIsCheckedOut] = React.useState(false);
+  const [isBreakedTime, setIsBreakedTime] = React.useState(false);
   const checkInTime = "09:00 AM";
   const checkOutTime = "05:00 PM";
+  const breakTime = "12:00 PM"; // Corrected variable name from BreakTime to breakTime
 
   if (!fontsLoaded) {
     return null;
@@ -58,7 +60,7 @@ const DashboardScreen = () => {
       {/* Today Attendance Section */}
       <View className="bg-gray-100 mt-8 rounded-3xl flex-1 pl-4 py-2">
         <Text className="text-left font-bold text-md">Today Attendance</Text>
-        <View className="flex-row justify-between p-4 mt-1">
+        <View className="flex-row flex-wrap justify-between p-4 mt-1">
           {/* Check In Card */}
           <CardForAttendance
             title="Check In"
@@ -69,7 +71,7 @@ const DashboardScreen = () => {
             isChecked={isCheckedIn}
             children="08:45 am"
             onPress={() => setIsCheckedIn(!isCheckedIn)}
-            marginRight="mr-2"
+            style={{ width: "48%", marginBottom: 10 }}
           />
 
           {/* Check Out Card */}
@@ -82,7 +84,34 @@ const DashboardScreen = () => {
             isChecked={isCheckedOut}
             children="17:00 pm"
             onPress={() => setIsCheckedOut(!isCheckedOut)}
-            marginLeft="mr-4"
+            style={{ width: "48%", marginBottom: 20 }}
+          />
+        </View>
+        <View className="flex-row flex-wrap justify-between p-4">
+          {/* Break Time */}
+          <CardForAttendance
+            title="Break Time"
+            iconSource={require("../assets/img/cup.png")}
+            backgroundColor="#F5F7F8"
+            time={breakTime} // Corrected variable name here
+            defaultText="Happy lunch!"
+            isChecked={isBreakedTime}
+            children="12:00 pm"
+            onPress={() => setIsBreakedTime(!isBreakedTime)}
+            style={{ width: "48%", marginBottom: 10 }}
+          />
+
+          {/* Total Days */}
+          <CardForAttendance
+            title="Total Days"
+            iconSource={require("../assets/img/calendar.png")}
+            backgroundColor="#F5F7F8"
+            time={checkOutTime}
+            defaultText="Working Days"
+            isChecked={isCheckedOut}
+            children="28 Days"
+            onPress={() => setIsCheckedOut(!isCheckedOut)}
+            style={{ width: "48%", marginBottom: 20 }}
           />
         </View>
       </View>
