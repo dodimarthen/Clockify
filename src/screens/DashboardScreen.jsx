f03f516f53ea86948059a7c1b1bd5c715d93ee10
dodimarthen <dodimartin.sit@gmail.com>
2024-09-25 09:40:31 +0700

    Add notification icon

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -5,6 +5,7 @@ import { useFonts } from "expo-font";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CalendarComponent from "../components/Calendar/Calendar";
 import CardForAttendance from "../components/Calendar/CardForAttendance";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const DashboardScreen = () => {
   const [fontsLoaded] = useFonts({
@@ -24,18 +25,28 @@ const DashboardScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       {/* Header Section */}
-      <View className="flex-row items-center p-4 mt-2">
-        <Avatar.Image
-          size={50}
-          source={{
-            uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4",
-          }}
-        />
-        <View className="ml-2">
-          <Text className="font-roboto text-lg font-bold">John Doe</Text>
-          <Text className="font-montserrat text-sm text-gray-500">
-            Software Engineer
-          </Text>
+      <View className="flex-row items-center justify-between p-4 mt-2">
+        <View className="flex-row items-center">
+          <Avatar.Image
+            size={50}
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4",
+            }}
+          />
+          <View className="ml-2">
+            <Text className="font-roboto text-lg font-bold">John Doe</Text>
+            <Text className="font-montserrat text-sm text-gray-500">
+              Software Engineer
+            </Text>
+          </View>
+        </View>
+
+        {/* Notification Icon with background */}
+        <View
+          style={{ backgroundColor: "rgba(240, 235, 227, 0.25)" }}
+          className="p-2 rounded-full"
+        >
+          <MaterialIcons name="notifications-none" size={24} color="black" />
         </View>
       </View>
 
