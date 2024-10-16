ca3f678a3d67bd08528c165475b566280e9c6781
dodimarthen <dodimartin.sit@gmail.com>
2024-10-16 09:39:08 +0700

    create button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
-function Button() {
+function Button({ text, onPress, color }) {
   return (
     <View className="items-center mt-5">
-      <TouchableOpacity className="bg-blue-500 hover:bg-blue-700 rounded-lg py-4 px-8">
-        <Text className="text-white font-bold">Sign Out</Text>
+      <TouchableOpacity
+        className={`${color} hover:bg-blue-700 rounded-lg py-4 px-8`}
+        onPress={onPress}
+      >
+        <Text className="text-white font-bold">{text}</Text>
       </TouchableOpacity>
     </View>
   );
