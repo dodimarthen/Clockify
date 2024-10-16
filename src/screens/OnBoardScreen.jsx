f43c5bd415a1caa33c681cc50f55761085bce9a8
dodimarthen <dodimartin.sit@gmail.com>
2024-10-16 09:39:32 +0700

    call the button component

--- src/screens/OnBoardScreen.jsx
+++ src/screens/OnBoardScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Image, Text, Pressable, Alert } from "react-native";
 import { useFonts } from "expo-font";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Button from "../components/Button/Button";
 
 const OnBoardScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
@@ -12,37 +13,28 @@ const OnBoardScreen = ({ navigation }) => {
     <SafeAreaView>
       <View className="flex-1 bg-custom-white justify-center items-center min-h-screen">
         <Text
-          className="text-2xl text-custom-blue font-semibold mt-1 mb-0"
+          className="text-2xl text-custom-blue font-semibold mt-1 mb-[-20]"
           style={{
             fontFamily: fontsLoaded ? "Roboto-Bold" : "System",
           }}
         >
           Welcome To
         </Text>
-        <Image source={require("../assets/img/Group 6.png")} className="mb-4" />
+        <Image
+          source={require("../assets/img/On.Time5.png")}
+          className="mb-2 h-20 w-40"
+        />
 
         <Image
           source={require("../assets/img/lol.png")}
           className="h-64 w-64 mb-20 mt-20"
         />
 
-        <Pressable
+        <Button
+          color="bg-custom-black"
+          text="Get Started"
           onPress={() => navigation.navigate("LoginPage")}
-          onPressIn={() => setPressed(true)}
-          onPressOut={() => setPressed(false)}
-          className={`items-center justify-center py-3 px-5 rounded-xl bg-custom-blue ${
-            pressed ? "opacity-40" : "opacity-200"
-          }`}
-        >
-          <Text
-            className="text-custom-white font-semibold text-base antialiased tracking-wider"
-            style={{
-              fontFamily: fontsLoaded ? "Roboto-Bold" : "System",
-            }}
-          >
-            Get Started!
-          </Text>
-        </Pressable>
+        />
       </View>
     </SafeAreaView>
   );
