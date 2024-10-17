72b3c72a775ccea0fa801298c885f602a6d2b03b
dodimarthen <dodimartin.sit@gmail.com>
2024-10-17 14:46:35 +0700

    create POST register

---
+++ Backend/src/service/user-service.js
@@ -0,0 +1,38 @@
+import { validate } from "../validation/validation.js";
+import { registerUserValidation } from "../validation/user-validation.js";
+import { prismaClient } from "../application/database.js";
+import { ResponseError } from "../error/response-error.js";
+import bcrypt from "bcrypt";
+
+const register = async (request) => {
+  try {
+    const user = validate(registerUserValidation, request);
+
+    const countUser = await prismaClient.user.count({
+      where: {
+        username: user.username,
+      },
+    });
+
+    if (countUser === 1) {
+      throw new ResponseError(400, "Username already exists");
+    }
+
+    user.password = await bcrypt.hash(user.password, 10);
+
+    const newUser = await prismaClient.user.create({
+      data: user,
+      select: {
+        username: true,
+        name: true,
+      },
+    });
+
+    return newUser;
+  } catch (error) {
+    console.error("Error during user registration:", error.message);
+    throw error;
+  }
+};
+
+export default { register };
