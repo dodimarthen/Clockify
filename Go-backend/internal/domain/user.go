55412aa7813b631dbf72dbdfa38823990e79c29a
dodimarthen <dodimartin.sit@gmail.com>
2024-10-17 08:50:46 +0700

    remove backend that using go

--- Go-backend/internal/domain/user.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Username string `json:"username"`
-	Role     string `json:"role"`
-}
