b647c46142e40db428848edd8dd0ac5ba52edd6a
dodimarthen <dodimartin.sit@gmail.com>
2024-10-24 15:32:24 +0700

    allow string to be null or empty in joi

--- Backend/src/validation/user-validation.js
+++ Backend/src/validation/user-validation.js
@@ -5,7 +5,7 @@ const registerUserValidation = Joi.object({
   password: Joi.string().max(100).required(),
   name: Joi.string().max(60).required(),
   email: Joi.string().max(200).required(),
-  role: Joi.string().max(60).required(),
+  role: Joi.string().max(60).allow("").required(),
 });
 
 const loginUserValidation = Joi.object({
