e86eb107b0a62d218fbd4610e015eb97cad8de0d
dodimarthen <dodimartin.sit@gmail.com>
2024-10-24 10:02:19 +0700

    add getcurrentuser function

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,6 +1,31 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const getCurrentUser = async (navigation) => {
+  try {
+    const token = await AsyncStorage.getItem("token");
+
+    if (!token) {
+      console.error("No token found, unable to fetch current user");
+      return;
+    }
+
+    const response = await axios.get(
+      `${process.env.EXPO_PUBLIC_API_URL}/api/users/current`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    if (response.data) {
+      console.log("Current User:", response.data);
+    }
+  } catch (error) {
+    console.error("Error fetching current user:", error);
+  }
+};
 export const handleLogin = async (
   email,
   password,
@@ -26,6 +51,10 @@ export const handleLogin = async (
     if (response.data && response.data.data && response.data.data.token) {
       await AsyncStorage.removeItem("token");
       await AsyncStorage.setItem("token", response.data.data.token);
+      console.log(response.data.data.token);
+
+      await getCurrentUser(navigation);
+
       navigation.navigate("Dashboard");
     } else {
       setLoginError("Invalid email or password.");
