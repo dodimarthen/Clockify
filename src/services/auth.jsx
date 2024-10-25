6abfd6ed13adb68c027aa61fcc89202426c39416
dodimarthen <dodimartin.sit@gmail.com>
2024-10-25 09:29:16 +0700

    fixing minor prob

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const getCurrentUser = async (navigation) => {
+export const getCurrentUser = async () => {
   try {
     const token = await AsyncStorage.getItem("token");
 
