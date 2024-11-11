0aaf88d115d94aad2b7540e7dcd6acb7e51751b0
dodimarthen <dodimartin.sit@gmail.com>
2024-11-11 09:20:52 +0700

    add default font

--- App.jsx
+++ App.jsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Montserrat-Black": require("./src/assets/fonts/Montserrat-Black.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
       <AppNavigator />
