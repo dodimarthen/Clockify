4274435ec893f73c7aca13f604390117edc2ebd2
dodimarthen <dodimartin.sit@gmail.com>
2024-11-12 15:45:10 +0700

    improve the order of navigation tabs

--- src/components/Panels/UserPanel.jsx
+++ src/components/Panels/UserPanel.jsx
@@ -5,26 +5,15 @@ import DashboardScreen from "../../screens/DashboardScreen";
 import homeIcon from "../../assets/img/home.png";
 import settingIcon from "../../assets/img/gear.png";
 import profileIcon from "../../assets/img/profile.png";
+import exploreIcon from "../../assets/img/compass.png";
 
 const Tab = createBottomTabNavigator();
-
-const HomeScreen = () => (
-  <View className="flex-1 justify-center items-center bg-gray-100">
-    <Text className="text-lg font-semibold text-indigo-500">Home Screen</Text>
-  </View>
-);
 
 const SettingsScreen = () => (
   <View className="flex-1 justify-center items-center bg-gray-100">
     <Text className="text-lg font-semibold text-green-500">
       Settings Screen
     </Text>
-  </View>
-);
-
-const ListScreen = () => (
-  <View className="flex-1 justify-center items-center bg-gray-100">
-    <Text className="text-lg font-semibold text-orange-500">List Screen</Text>
   </View>
 );
 
@@ -36,12 +25,19 @@ const ProfileScreen = () => (
   </View>
 );
 
+const ExploreScreen = () => (
+  // New Explore screen
+  <View className="flex-1 justify-center items-center bg-gray-100">
+    <Text className="text-lg font-semibold text-blue-500">Explore Screen</Text>
+  </View>
+);
+
 const UserPanel = () => {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color, size }) => {
-          const iconSize = 28;
+          const iconSize = 25;
 
           if (route.name === "Home") {
             return (
@@ -79,15 +75,28 @@ const UserPanel = () => {
                 resizeMode="contain"
               />
             );
+          } else if (route.name === "Explore") {
+            return (
+              <Image
+                source={exploreIcon}
+                style={{
+                  width: iconSize,
+                  height: iconSize,
+                  tintColor: color,
+                }}
+                resizeMode="contain"
+              />
+            );
           }
         },
         tabBarActiveTintColor: "#091057",
         tabBarInactiveTintColor: "gray",
         tabBarStyle: { backgroundColor: "white", paddingBottom: 1 },
-        headerShown: false, // No header in tab navigator
+        headerShown: false,
       })}
     >
       <Tab.Screen name="Home" component={DashboardScreen} />
+      <Tab.Screen name="Explore" component={ExploreScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
