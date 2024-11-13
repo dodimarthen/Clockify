863ffa15006353b52a798d3e6b813aa83c5904eb
dodimarthen <dodimartin.sit@gmail.com>
2024-11-13 11:29:22 +0700

    initialize recordpage to stack navigator

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -4,8 +4,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnBoardScreen from "../screens/OnBoardScreen";
 import LoginScreen from "../screens/LoginScreen";
 import DashboardScreen from "../screens/DashboardScreen";
-
+import AdminPanel from "../components/Panels/AdminPanel";
 import ForgetPasswordScreen from "../screens/ForgetPasswordScreen";
+import UserPanel from "../components/Panels/UserPanel";
+import AttendanceRecordPage from "../components/Pages/AttendanceRecordPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +19,12 @@ const AppNavigator = () => (
     <Stack.Screen name="Dashboard" component={DashboardScreen} />
     <Stack.Screen name="LoginPage" component={LoginScreen} />
     <Stack.Screen name="OnBoard" component={OnBoardScreen} />
+    <Stack.Screen name="AdminPanel" component={AdminPanel} />
+    <Stack.Screen name="UserPanel" component={UserPanel} />
+    <Stack.Screen
+      name="AttendanceRecordPage"
+      component={AttendanceRecordPage}
+    />
     <Stack.Screen name="ForgetPassword" component={ForgetPasswordScreen} />
   </Stack.Navigator>
 );
